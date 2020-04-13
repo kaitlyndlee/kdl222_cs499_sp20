483938f9e4d0f2e63ec035667948fa7d420963d3
Kaitlyn <kdl222@nau.edu>
2020-04-12 20:40:28 -0700

    Changed test.

--- assignment3/symexwins.cpp
+++ assignment3/symexwins.cpp
@@ -5,71 +5,41 @@
 
 using namespace deepstate;
 
-float **gauss_jordan(int sizeOfMatrix, float **input, float **inverse) {
-    float localVariable;
-    int i,j,k;
 
-    for(i=0;i<sizeOfMatrix;i++) {                                    
-      for(j=0;j<sizeOfMatrix;j++) {                          
-        if(i==j) {                                      
-          inverse[i][j]=1;  
-        }                                  
-        else {                                           
-           inverse[i][j]=0;
-        }                               
-      }
-    } 
-
-
-    for(k=0;k<sizeOfMatrix;k++) {                                                        
-        localVariable=input[k][k];                                        
-        for(j=0;j<sizeOfMatrix;j++) {
-            input[k][j]/=localVariable;                                    
-            inverse[k][j]/=localVariable;                                    
-
-        }                                                    
-        for(i=0;i<sizeOfMatrix;i++) {
-            localVariable = input[i][k];                                    
-            for(j=0;j<sizeOfMatrix;j++) {                                                
-                if(i==k)
-                    break;                                    
-                input[i][j] -= input[k][j]*localVariable;                        
-                inverse[i][j] -= inverse[k][j]*localVariable;                        
-            }
-        }
-  }
-  return inverse;
-}
-
-
-
-
-TEST(StrUtil, StrManipulation) {
-  float **input;
-  float **inverse;
-  int i,j,k;
-  int sizeOfMatrix = 2;
-
-  input = (float **)malloc(sizeOfMatrix*sizeof(float *)); 
-  for(i=0;i<sizeOfMatrix;i++) {
-    input[i]=(float *)malloc(sizeOfMatrix*sizeof(float));
-  }
-
-  inverse=(float **)malloc(sizeOfMatrix*sizeof(float *));        
-  for(i=0;i<sizeOfMatrix;i++) {
-
-    inverse[i]=(float *)malloc(sizeOfMatrix*sizeof(float));
-  }
-
-  for(i=0;i<sizeOfMatrix;i++) {
-    for(j=0;j<sizeOfMatrix;j++) {
-      input[i][j] = DeepState_UIntInRange(1, 10000);
+int isHadamard(int sizeOfMatrix, float inputMatrix[2][2]) {
+  float transpose[2][2];
+  for(int i = 0; i < sizeOfMatrix; i++) {                                    
+    for(int j = 0; j < sizeOfMatrix; j++) {                          
+      transpose[j][i] = inputMatrix[i][j];
     }
   }
+
+  int i, j, k; 
+  float result[2][2];
+  for (i = 0; i < sizeOfMatrix; i++) { 
+    for (j = 0; j < sizeOfMatrix; j++) { 
+      result[i][j] = 0; 
+      for (k = 0; k < sizeOfMatrix; k++) {
+        result[i][j] += inputMatrix[i][k] * transpose[k][j]; 
+      } 
+    }
+  } 
+
+  return ( (result[0][0] == 1) && (result[0][1] == 0) && (result[1][0] == 0) && (result[1][1] == 1));
+}
+
+int main()
+
+TEST(StrUtil, StrManipulation) {
+  float input[2][2];
+  int i,j,k;
+
+  for(i = 0; i < 2; i++) {
+    for(j = 0; j < 2; j++) {
+      input[i][j] =  DeepState_IntInRange(-1, 1);;
+    }
+  }
+  isHadamard(2, input);
   LOG(TRACE)<< printf("Input matrix: [[%f, %f], [%f, %f]]\n", input[0][0], input[0][1], input[1][0], input[1][1]);
-  gauss_jordan(sizeOfMatrix, input, inverse);
-  ASSERT_NE(inverse[0][0], 1);
-  ASSERT_NE(inverse[0][1], 2);
-  ASSERT_NE(inverse[1][0], 1);
-  ASSERT_NE(inverse[1][1], 2);
+  ASSERT_EQ(isHadamard(2, input), 1);
 }
