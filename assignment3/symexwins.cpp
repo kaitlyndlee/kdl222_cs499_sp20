5466e9d45c3c733704a1564ee037fe6c854bacd2
Kaitlyn <kdl222@nau.edu>
2020-04-12 20:50:08 -0700

    Easier example.

--- assignment3/symexwins.cpp
+++ assignment3/symexwins.cpp
@@ -6,40 +6,25 @@
 using namespace deepstate;
 
 
-int isHadamard(int sizeOfMatrix, float inputMatrix[2][2]) {
-  float transpose[2][2];
-  for(int i = 0; i < sizeOfMatrix; i++) {                                    
-    for(int j = 0; j < sizeOfMatrix; j++) {                          
-      transpose[j][i] = inputMatrix[i][j];
-    }
-  }
+int isPrime(unsigned int number) {
+  int flag = 1;
+  // Iterate from 2 to n/2 
+  for (i = 2; i <= sqrt(n); i++) { 
 
-  int i, j, k; 
-  float result[2][2];
-  for (i = 0; i < sizeOfMatrix; i++) { 
-    for (j = 0; j < sizeOfMatrix; j++) { 
-      result[i][j] = 0; 
-      for (k = 0; k < sizeOfMatrix; k++) {
-        result[i][j] += inputMatrix[i][k] * transpose[k][j]; 
-      } 
-    }
+    // If n is divisible by any number between 
+    // 2 and n/2, it is not prime 
+    if (n % i == 0) { 
+      flag = 0; 
+      break; 
+    } 
   } 
-
-  return ( (result[0][0] == 1) && (result[0][1] == 0) && (result[1][0] == 0) && (result[1][1] == 1));
+  return flag;
 }
 
 int main()
 
 TEST(StrUtil, StrManipulation) {
-  float input[2][2];
-  int i,j,k;
-
-  for(i = 0; i < 2; i++) {
-    for(j = 0; j < 2; j++) {
-      input[i][j] =  DeepState_IntInRange(-1, 1);;
-    }
-  }
-  isHadamard(2, input);
-  LOG(TRACE)<< printf("Input matrix: [[%f, %f], [%f, %f]]\n", input[0][0], input[0][1], input[1][0], input[1][1]);
-  ASSERT_EQ(isHadamard(2, input), 1);
+  int inputNum =DeepState_UIntInRange(0, 100000);
+  LOG(TRACE)<< printf("Input number: %f", inputNum);
+  ASSERT_EQ(isPrime(inputNum), 1);
 }
