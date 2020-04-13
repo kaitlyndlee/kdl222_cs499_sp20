b56ec18c6824002bb0863dd4fe87ba5639b78f55
Kaitlyn Lee <kdl222@nau.edu>
2020-04-13 04:29:36 +0000

    Fixed errors.

--- assignment3/symexwins.cpp
+++ assignment3/symexwins.cpp
@@ -1,15 +1,15 @@
 #include <deepstate/DeepState.hpp>
 
-#include <iostream>
-#include <string>
+#include <stdio.h>
+#include <math.h>
 
 using namespace deepstate;
 
 
-int isPrime(unsigned int number) {
+int isPrime(unsigned int n) {
   int flag = 1;
   // Iterate from 2 to n/2 
-  for (i = 2; i <= sqrt(n); i++) { 
+  for (int i = 2; i <= sqrt(n); i++) { 
 
     // If n is divisible by any number between 
     // 2 and n/2, it is not prime 
@@ -21,10 +21,9 @@ int isPrime(unsigned int number) {
   return flag;
 }
 
-int main()
 
 TEST(StrUtil, StrManipulation) {
-  int inputNum =DeepState_UIntInRange(0, 100000);
-  LOG(TRACE)<< printf("Input number: %f", inputNum);
+  int inputNum =DeepState_UIntInRange(0, 10000);
+  LOG(TRACE)<< printf("Input number: %d", inputNum);
   ASSERT_EQ(isPrime(inputNum), 1);
 }
