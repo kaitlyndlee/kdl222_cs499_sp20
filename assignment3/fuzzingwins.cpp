8c9af58aa2107ff64d895eaa659d5d3660630c50
Kaitlyn <kdl222@nau.edu>
2020-04-14 11:16:39 -0700

    New test.

--- assignment3/fuzzingwins.cpp
+++ assignment3/fuzzingwins.cpp
@@ -1,80 +1,28 @@
 #include <deepstate/DeepState.hpp>
 
-#include <stdbool.h> 
-#include <string.h>
+// #include <string>
 using namespace deepstate;
 
-size_t length_of_buffer(const void *buf) {
-    size_t length = 0;
-    const unsigned char *buf_c = (const unsigned char *) buf;
-    int i = 0 ;
-    while(buf_c[i]) {
-        length++;
-        i++;
-    }
-    return length;
+char lowercase(char c) {
+    if(c >= 'A' && c <= 'Z')
+        return 'a' + (c - 'A');
+    else
+        return c;
 }
 
-int is_binary(const void *buf) {
-    size_t suspicious_bytes = 0;
-    const unsigned char *buf_c = (const unsigned char *) buf;
-    size_t buf_len = length_of_buffer(buf_c);
-    size_t total_bytes = buf_len > 512 ? 512 : buf_len;
-    
-    size_t i;
-
-    if (buf_len == 0) {
-        /* Is an empty file binary? Is it text? */
-        return 0;
+char strcmp_nocase(const char* a, const char* b) {
+    while(lowercase(*a) == lowercase(*b)) {
+        if(*a == 0)
+            return 0;
+        ++a; ++b;
     }
 
-    if (buf_len >= 3 && buf_c[0] == 0xEF && buf_c[1] == 0xBB && buf_c[2] == 0xBF) {
-        /* UTF-8 BOM. This isn't binary. */
-        return 0;
-    }
-
-    if (buf_len >= 5 && strncmp((const char *) buf, "%PDF-", 5) == 0) {
-        /* PDF. This is binary. */
-        return 1;
-    }
-
-    for (i = 0; i < total_bytes; i++) {
-        if (buf_c[i] == '\0') {
-            /* NULL char. It's binary */
-            return 1;
-        } else if ((buf_c[i] < 7 || buf_c[i] > 14) && (buf_c[i] < 32 || buf_c[i] > 127)) {
-            /* UTF-8 detection */
-            if (buf_c[i] > 193 && buf_c[i] < 224 && i + 1 < total_bytes) {
-                i++;
-                if (buf_c[i] > 127 && buf_c[i] < 192) {
-                    continue;
-                }
-            } else if (buf_c[i] > 223 && buf_c[i] < 240 && i + 2 < total_bytes) {
-                i++;
-                if (buf_c[i] > 127 && buf_c[i] < 192 && buf_c[i + 1] > 127 && buf_c[i + 1] < 192) {
-                    i++;
-                    continue;
-                }
-            }
-            suspicious_bytes++;
-            /* Disk IO is so slow that it's worthwhile to do this calculation after every suspicious byte. */
-            /* This is true even on a 1.6Ghz Atom with an Intel 320 SSD. */
-            /* Read at least 32 bytes before making a decision */
-            if (i >= 32 && (suspicious_bytes * 100) / total_bytes > 10) {
-                return 1;
-            }
-        }
-    }
-    if ((suspicious_bytes * 100) / total_bytes > 10) {
-        return 1;
-    }
-
-    return 0;
+    return *a-*b;
 }
 
 TEST(Fuzzingwins, StrTest) {
-  char* inputString = DeepState_CStr_C(128, " abcdefghijklmnopqrstuvwxyz0123456789-_()/?*&%$#@![]{}");
+  char* inputString = DeepState_CStr_C(128, " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
   LOG(TRACE) << "String: " << inputString;
-  ASSERT_FALSE(is_binary(inputString));
+  ASSERT_FALSE(strcmp_nocase(inputString, "i like pizza"));
 }
 
