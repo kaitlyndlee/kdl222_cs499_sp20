9835f3fe95927069bfe01c42a971a5ee34897f86
Kaitlyn <kdl222@nau.edu>
2020-04-20 11:34:07 -0700

    Added is_binary call.

--- Final_Project/Tests.cpp
+++ Final_Project/Tests.cpp
@@ -77,71 +77,75 @@ TEST(StrUtil, StrManipulation) {
 
     ASSERT_TRUE(stringReplaced(originalStr, inputStr, substr, newStr, substrStart, substrEnd));
 
-//     // OneOf
-//     stringSize = DeepState_SizeInRange(2, 32);
-//     LOG(TRACE) << "String Size: " << stringSize;
-//     inputChar = DeepState_CStr_C(stringSize, " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_()/?*&%$#@![]{}");
-//     std::string originalStrLong(inputChar);
-//     inputStr = originalStrLong;
+    // Check if the string is binary, but do not assert
+    int returnVal = is_binary(inputStr);
+    LOG(TRACE) << "Is binary: " << returnVal;
 
-//     for (int n = 0; n < stringSize; n++) {
-//         LOG(TRACE) << "Number of Iterations: " << n;
-//         OneOf(
-//           [&] {
-//             LOG(TRACE) << "Started Lowercase: " << inputStr;
-//             toLowercase(inputStr);
+    // OneOf
+    stringSize = DeepState_SizeInRange(2, 32);
+    LOG(TRACE) << "String Size: " << stringSize;
+    inputChar = DeepState_CStr_C(stringSize, " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_()/?*&%$#@![]{}");
+    std::string originalStrLong(inputChar);
+    inputStr = originalStrLong;
 
-//             ASSERT_TRUE(isLower(originalStrLong, inputStr));
-//             originalStrLong = inputStr;
-//             LOG(TRACE) << "Our Lowercase: " << inputStr;
-//           },
-//           [&] {
-//             LOG(TRACE) << "Started Uppercase: " << inputStr;
-//             toUppercase(inputStr);
+    for (int n = 0; n < stringSize; n++) {
+        LOG(TRACE) << "Number of Iterations: " << n;
+        OneOf(
+          [&] {
+            LOG(TRACE) << "Started Lowercase: " << inputStr;
+            toLowercase(inputStr);
 
-//             ASSERT_TRUE(isUpper(originalStrLong, inputStr));
-//             originalStrLong = inputStr;
-//             LOG(TRACE) << "Our Uppercase: " << inputStr;
-//           },
-//         [&] {
-//             LOG(TRACE) << "Started Trim: " << inputStr;
-//             inputStr = trim(inputStr);
-//             originalStrLong = inputStr;
-//             LOG(TRACE) << "Trimmed whitespace";
-//           },
-//         [&] {
-//             LOG(TRACE) << "Started Char Trim: " << inputStr;
-//             char *charToTrim = DeepState_CStrUpToLen(1);
-//             inputStr = trim(inputStr, charToTrim);
-//             originalStrLong = inputStr;
-//             LOG(TRACE) << "Trimmed char: "<< charToTrim << " string: " << inputStr;
-//           },
-//         [&] {
-//             LOG(TRACE) << "Started Replace: " << inputStr;
-//             int substrStart = 0;
-//             int substrEnd = 1;
-//             if(stringSize > 2) {
-//                 substrStart = DeepState_IntInRange(0, stringSize - 2);
-//                 substrEnd = DeepState_IntInRange(substrStart + 1, stringSize - 1);
-//             }
+            ASSERT_TRUE(isLower(originalStrLong, inputStr));
+            originalStrLong = inputStr;
+            LOG(TRACE) << "Our Lowercase: " << inputStr;
+          },
+          [&] {
+            LOG(TRACE) << "Started Uppercase: " << inputStr;
+            toUppercase(inputStr);
 
-//             char* newStringChar = DeepState_CStr_C(substrEnd - substrStart, " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_()/?*&%$#@![]{}");
-//             std::string newStr(newStringChar);
-//             std::string substr = inputStr.substr(substrStart, substrEnd - substrStart);
-//             inputStr = replace(inputStr, substr, newStr);
+            ASSERT_TRUE(isUpper(originalStrLong, inputStr));
+            originalStrLong = inputStr;
+            LOG(TRACE) << "Our Uppercase: " << inputStr;
+          },
+        [&] {
+            LOG(TRACE) << "Started Trim: " << inputStr;
+            inputStr = trim(inputStr);
+            originalStrLong = inputStr;
+            LOG(TRACE) << "Trimmed whitespace";
+          },
+        [&] {
+            LOG(TRACE) << "Started Char Trim: " << inputStr;
+            char *charToTrim = DeepState_CStrUpToLen(1);
+            inputStr = trim(inputStr, charToTrim);
+            originalStrLong = inputStr;
+            LOG(TRACE) << "Trimmed char: "<< charToTrim << " string: " << inputStr;
+          },
+        [&] {
+            LOG(TRACE) << "Started Replace: " << inputStr;
+            int substrStart = 0;
+            int substrEnd = 1;
+            if(stringSize > 2) {
+                substrStart = DeepState_IntInRange(0, stringSize - 2);
+                substrEnd = DeepState_IntInRange(substrStart + 1, stringSize - 1);
+            }
 
-//             ASSERT_TRUE(stringReplaced(originalStrLong, inputStr, substr, newStr, substrStart, substrEnd));
-//             originalStrLong = inputStr;
-//             LOG(TRACE) << "Replaced substring: "<< substr << " with " <<  newStr << " string:" << inputStr;
-//           },
-//         [&] {
-//             LOG(TRACE) << "Started Replace Char: " << inputStr;
-//             char newChar = DeepState_Char();
-//             char charToReplace = DeepState_Char();
-//             replaceChar(inputStr, charToReplace, newChar);
-//             originalStrLong = inputStr;
-//             LOG(TRACE) << "Replaced char: " << charToReplace << " with: " << newChar << " string: " << inputStr;
-//           }
-//         );
-//     }
+            char* newStringChar = DeepState_CStr_C(substrEnd - substrStart, " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_()/?*&%$#@![]{}");
+            std::string newStr(newStringChar);
+            std::string substr = inputStr.substr(substrStart, substrEnd - substrStart);
+            inputStr = replace(inputStr, substr, newStr);
+
+            ASSERT_TRUE(stringReplaced(originalStrLong, inputStr, substr, newStr, substrStart, substrEnd));
+            originalStrLong = inputStr;
+            LOG(TRACE) << "Replaced substring: "<< substr << " with " <<  newStr << " string:" << inputStr;
+          },
+        [&] {
+            LOG(TRACE) << "Started Replace Char: " << inputStr;
+            char newChar = DeepState_Char();
+            char charToReplace = DeepState_Char();
+            replaceChar(inputStr, charToReplace, newChar);
+            originalStrLong = inputStr;
+            LOG(TRACE) << "Replaced char: " << charToReplace << " with: " << newChar << " string: " << inputStr;
+          }
+        );
+    }
 }
